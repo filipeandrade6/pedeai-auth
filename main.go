90a24d015b9ea464a8b53b5e32de862f5edcc00b
Filipe Andrade <filipe.engenharia42@gmail.com>
2024-09-19 20:03:30 -0300

    formatted files

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,12 +12,29 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/jackc/pgx/v5"
-	_ "github.com/lib/pq"
 )
 
-func handleRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+type User struct {
+	CPF string `json:"cpf"`
+}
 
-	log.Println("entrou na lambda!")
+func handleRequest(req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
+
+	if req.RequestContext.HTTP.Method != "POST" {
+		return events.APIGatewayV2HTTPResponse{
+			StatusCode: http.StatusMethodNotAllowed,
+			Body:       http.StatusText(http.StatusMethodNotAllowed),
+		}, nil
+	}
+
+	var user User
+	err := json.Unmarshal([]byte(req.Body), &user)
+	if err != nil {
+		return events.APIGatewayV2HTTPResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body:       err.Error(),
+		}, nil
+	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Second*15))
 	defer cancel()
@@ -29,7 +47,6 @@ func handleRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyRes
 
 	// urlExample := "postgres://username:password@localhost:5432/database_name"
 	var url = fmt.Sprintf("postgres://%s:%s@%s:%s/%s", usuario, senha, host, port, db)
-	log.Println(url)
 
 	conn, err := pgx.Connect(ctx, url)
 	if err != nil {
@@ -37,26 +54,28 @@ func handleRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyRes
 	}
 	defer conn.Close(context.Background())
 
-	log.Print("conectou!")
-
 	var id string
-
-	err = conn.QueryRow(context.Background(), "select id from clientes where nome=$1", "FILIPE ANDRADE").Scan(&id)
+	err = conn.QueryRow(context.Background(), "SELECT id FROM clientes WHERE cpf=$1", user.CPF).Scan(&id)
 	if err != nil {
-		log.Fatalf("QueryRow failed: %v\n", err)
+		if err == pgx.ErrNoRows {
+			return events.APIGatewayV2HTTPResponse{
+				StatusCode: http.StatusNotFound,
+				Headers:    map[string]string{"Content-Type": "text/plain; charset=utf-8"},
+				Body:       "CPF não encontrado",
+			}, nil
+		}
+		return events.APIGatewayV2HTTPResponse{
+			StatusCode: http.StatusInternalServerError,
+			Headers:    map[string]string{"Content-Type": "text/plain; charset=utf-8"},
+			Body:       err.Error(),
+		}, nil
 	}
 
-	log.Print("executou a query!")
-
-	fmt.Println(id)
-
-	res := events.APIGatewayProxyResponse{
+	return events.APIGatewayV2HTTPResponse{
 		StatusCode: http.StatusOK,
 		Headers:    map[string]string{"Content-Type": "text/plain; charset=utf-8"},
 		Body:       id,
-	}
-
-	return res, nil
+	}, nil
 }
 
 func main() {
